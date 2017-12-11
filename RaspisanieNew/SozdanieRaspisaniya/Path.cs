c4e88f7d36213e34daf430dcad63639132dc4d31
ArtemMaslow <32580687+ArtemMaslow@users.noreply.github.com>
2017-12-12 00:05:00 +0300

    XML file

--- RaspisanieNew/SozdanieRaspisaniya/Path.cs
+++ RaspisanieNew/SozdanieRaspisaniya/Path.cs
@@ -2,11 +2,11 @@
 {
     public static class Path
     {
-        public static string FacultyXml = "E:\\Kursach\\Faculty.xml";
-        public static string ClassroomXml = "E:\\Kursach\\Classroom.xml";
-        public static string DepartmentXml = "E:\\Kursach\\Department.xml";
-        public static string GroupXml = "E:\\Kursach\\Group.xml";
-        public static string TeacherXml = "E:\\Kursach\\Teacher.xml";
-        public static string SubjectXml = "E:\\Kursach\\Subject.xml";
+        public static string FacultyXml = @"E:\Downloads\RaspisanieNew\Faculty.xml";
+        public static string ClassroomXml = @"E:\Downloads\RaspisanieNew\Classroom.xml";
+        public static string DepartmentXml = @"E:\Downloads\RaspisanieNew\Department.xml";
+        public static string GroupXml = @"E:\Downloads\RaspisanieNew\Group.xml";
+        public static string TeacherXml = @"E:\Downloads\RaspisanieNew\Teacher.xml";
+        public static string SubjectXml = @"E:\Downloads\RaspisanieNew\Subject.xml";
     }
 }
