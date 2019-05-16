7d97873f3be62c838e46e07785473656d8d3e92b
ArtemMaslow <32580687+ArtemMaslow@users.noreply.github.com>
2019-05-16 21:48:24 +0300

    Update Windows.cs

--- RaspisanieNew2/SozdanieRaspisaniya/ViewModel/Rules/Windows.cs
+++ RaspisanieNew2/SozdanieRaspisaniya/ViewModel/Rules/Windows.cs
@@ -12,29 +12,38 @@ namespace SozdanieRaspisaniya.ViewModel.Rules
         public void Check(ObservableCollection<ObservableCollection<DropItem>> filtered, ref List<string> listOfErrors)
         {
             int day;
-            int tempday = 0;
+            int tempday = SheduleSettings.WeekDayMaxCount - 1;
+            int prevday = 0;
+            int firstPair = SheduleSettings.SaturdayMaxCount - 2;
+            bool hasWindows = false;
+
             for (int i = 0; i < filtered[0].Count; i++)
             {
-                for (int j = 2; j < filtered.Count; j++)
+                for (int j = filtered.Count - 1; j >= 0; j--)
                 {
-                    day = j / 6;
-                    if (day != tempday)
+                    if (j != 0)
                     {
-                        tempday = day;
-                        j += 2;
-                    }
+                        day = (int)filtered[j][i].Info.Day;
+                        prevday = (int)filtered[j - 1][i].Info.Day;
 
-                    if (filtered[j][i].Item.Subject != null && filtered[j - 1][i].Item.Subject == null && filtered[j - 2][i].Item.Subject != null && filtered[j][i].Item.Ndindex == 0 && filtered[j - 2][i].Item.Ndindex == 0)
-                    {
-                        message = string.Format("У группы {0} есть окно {1}!", filtered[j][i].Item.Group.Single().NameOfGroup, dayValue[day]);
-                        listOfErrors.Add(message);
-                    }
+                        if (day == prevday)
+                        {
+                            if (filtered[j][i].Item.Subject != null && filtered[j - 1][i].Item.Subject == null /*&& (filtered[j][i].Item.Ndindex == 0 || filtered[j][i].Item.Ndindex == 1)*/)
+                            {
+                                if (hasWindows)
+                                {
+                                    listOfErrors.Add(message);
+                                }
 
-                    //if (filtered[j][i].Item.Subject != null && filtered[j - 1][i].ItemTwo.Subject == null && filtered[j - 2][i].Item.Subject != null && filtered[j][i].Item.Ndindex == 0 && filtered[j - 2][i].Item.Ndindex == 0)
-                    //{
-                    //    message = string.Format("У группы {0} есть окно по четным неделям {1}!", filtered[j][i].Item.Group.Single().NameOfGroup, dayValue[day]);
-                    //    listOfErrors.Add(message);
-                    //}
+                                message = string.Format("У группы {0} есть окно {1}!", filtered[j][i].Item.Group.Single().NameOfGroup, dayValue[day - 1]);
+                                hasWindows = true;
+                            }
+                        }
+                        else
+                        {
+                            hasWindows = false;
+                        }
+                    }
                 }
             }
         }
